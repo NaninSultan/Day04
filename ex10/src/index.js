6c7532ff399989f33036aee10b95e9a8ab97349b
unknown <hamza.okanovic@hotmail.com>
2021-03-11 23:37:08 +0100

    Added index.js file

---
+++ ex10/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    //
+var myFirstName = "Hamza";
+var myLastName = "Okanovic";
+var myHomeTown = "Sarajevo";
+    //
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+console.log(main());
+module.exports = main;
